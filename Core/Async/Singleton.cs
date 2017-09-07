292277f8c62eff468838ad57ca88dcef453ff6e9
Kornél Regius <rkornel@hotmail.com>
2017-09-07 21:30:35 +0200

    Singleton fix

--- Core/Async/Singleton.cs
+++ Core/Async/Singleton.cs
@@ -30,12 +30,15 @@ namespace SampleHorse.Core.Async
             get
             {
                 if (_internalInstance != null) return _internalInstance;
+                //T1,T2
                 lock (_locker)
                 {
+                    //T1   //T2
                     if (_internalInstance != null) return _internalInstance;
 
                     var tempInstance = new Singleton();
                     Thread.MemoryBarrier();
+                    //T1
                     _internalInstance = tempInstance;
                 }
                 return _internalInstance;
@@ -53,10 +56,12 @@ namespace SampleHorse.Core.Async
     public class LazySingleton
     {
         private static readonly Lazy<LazySingleton> InternalInstance =
-            new Lazy<LazySingleton>(()=> new LazySingleton(),LazyThreadSafetyMode.PublicationOnly);
+            new Lazy<LazySingleton>(() => new LazySingleton(), LazyThreadSafetyMode.PublicationOnly);
 
         public static LazySingleton Instance => InternalInstance.Value;
 
         public int GlobalValue { get; set; }
+
+        private LazySingleton() { }
     }
 }
